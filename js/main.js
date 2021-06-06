6114b0ca5b1dad22ad3db36054ac18e93ee74341
malakabdelrazek <85433877+malakabdelrazek@users.noreply.github.com>
2021-06-06 10:56:11 +0200

    upload files= 1st time

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var getUsername = prompt ("Enter your name");
+
+console.log(getUsername);
+
+document.getElementById("username").innerText = getUsername;
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+
+  
